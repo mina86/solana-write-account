bfa612ad21153277be4577ce062685860eeda4a1
Michal Nazarewicz <mina86@mina86.com>
2025-07-29 16:34:21 +0200

    Prepare 0.2.0 relaese

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+// solana-write-account — helper program and library for handling Solana
+//                        transaction size limit
+// © 2024 by Composable Foundation
+// © 2025 by Michał Nazarewicz <mina86@mina86.com>
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, see <https://www.gnu.org/licenses/>.
+
 //! Helper smart contract and library functions to support calling Solana
 //! programs with instruction data read from an account.
 //!
