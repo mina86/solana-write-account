4c7dc547be4f6430a16a13c7904927032aa06397
Michal Nazarewicz <mina86@mina86.com>
2025-08-04 22:57:14 +0200

    Add tests for `deserialize` and `entrypoint!`

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -102,19 +102,16 @@ macro_rules! entrypoint {
         /// as done by the Solana runtime.
         #[no_mangle]
         pub unsafe extern "C" fn entrypoint(input: *mut u8) -> u64 {
-            // SAFETY: Caller promises this is safe.
-            let parsed = unsafe { $crate::entrypoint::deserialize(input) };
-            let (program_id, accounts, data) = match parsed {
-                Ok(it) => it,
-                Err(error) => return error.into(),
-            };
-            match $process_instruction(program_id, &accounts, data) {
-                Ok(()) => $crate::entrypoint::__private::SUCCESS,
-                Err(error) => error.into(),
+            // SAFETY: Caller guarantees it’s safe.
+            unsafe {
+                $crate::entrypoint::__private::entrypoint_impl(
+                    input,
+                    |pid, accs, data| $process_instruction(pid, &accs, data),
+                )
             }
         }
-        $crate::entrypoint::__private::solana_program::custom_heap_default!();
-        $crate::entrypoint::__private::solana_program::custom_panic_default!();
+        $crate::entrypoint::__private::custom_heap_default!();
+        $crate::entrypoint::__private::custom_panic_default!();
     };
 }
 
@@ -134,79 +131,301 @@ macro_rules! entrypoint_no_alloc {
         /// as done by the Solana runtime.
         #[no_mangle]
         pub unsafe extern "C" fn entrypoint(input: *mut u8) -> u64 {
-            use core::mem::MaybeUninit;
-
-            use $crate::entrypoint::__private;
-
-            let mut accounts =
-                [const { MaybeUninit::<__private::AccountInfo>::uninit() }; 64];
-            // SAFETY: Caller promises this is safe.
-            let parsed = unsafe {
-                $crate::entrypoint::deserialize_into(input, &mut accounts)
-            };
-            let (program_id, num_accounts, instruction_data) = match parsed {
-                Ok(it) => it,
-                Err(error) => return error.into(),
-            };
-            let accounts = &accounts[..num_accounts]
-                as *const [MaybeUninit<__private::AccountInfo>]
-                as *const [AccountInfo];
-            // SAFETY: deserialize_into initialised the first num_accounts
-            // entries of the array and `MU<X>` has the same layout as `X`.
-            let accounts = unsafe { &*accounts };
-
-            // Make sure we have a new stack frame.  Solana stack frame sizes
-            // are limited so the accounts array would eat into user’s available
-            // stack space.
-            #[inline(never)]
-            fn inner(
-                program_id: &__private::Pubkey,
-                accounts: &[AccountInfo],
-                data: &[u8],
-            ) -> u64 {
-                match $process_instruction(program_id, &accounts, data) {
-                    Ok(()) => __private::SUCCESS,
-                    Err(error) => error.into(),
-                }
+            // SAFETY: Caller guarantees it’s safe.
+            unsafe {
+                $crate::entrypoint::__private::entrypoint_no_alloc_impl(
+                    input,
+                    |pid, accs, data| $process_instruction(pid, accs, data),
+                )
             }
-
-            inner(program_id, accounts, instruction_data)
         }
-        $crate::entrypoint::__private::solana_program::custom_heap_default!();
-        $crate::entrypoint::__private::solana_program::custom_panic_default!();
+        $crate::entrypoint::__private::custom_heap_default!();
+        $crate::entrypoint::__private::custom_panic_default!();
     };
 }
 
 
 #[doc(hidden)]
 pub mod __private {
-    pub use solana_program;
-    pub use solana_program::account_info::AccountInfo;
-    pub use solana_program::entrypoint::SUCCESS;
-    pub use solana_program::pubkey::Pubkey;
-}
+    use core::mem::MaybeUninit;
 
-#[test]
-fn test_get_ix_data() {
-    let key = Pubkey::new_unique();
+    use solana_program::account_info::AccountInfo;
+    use solana_program::entrypoint::SUCCESS;
+    use solana_program::pubkey::Pubkey;
+    pub use solana_program::{custom_heap_default, custom_panic_default};
 
-    fn account_info<'a>(
-        key: &'a Pubkey,
-        lamports: &'a mut u64,
-        data: &'a mut [u8],
-    ) -> AccountInfo<'a> {
-        AccountInfo::new(key, false, false, lamports, data, key, false, 0)
+    type Result =
+        core::result::Result<(), solana_program::program_error::ProgramError>;
+
+    #[inline(always)]
+    pub unsafe fn entrypoint_impl(
+        input: *mut u8,
+        process: impl FnOnce(&Pubkey, Vec<AccountInfo>, &[u8]) -> Result,
+    ) -> u64 {
+        // SAFETY: Caller promises this is safe.
+        unsafe { super::deserialize(input) }
+            .and_then(|(pid, accs, data)| process(pid, accs, data))
+            .map_or_else(|error| error.into(), |()| SUCCESS)
     }
 
-    let check = |want, data: &[u8]| {
-        let mut lamports = 0u64;
-        let mut data = data.to_vec();
-        let acc = account_info(&key, &mut lamports, &mut data);
-        assert_eq!(want, get_ix_data(acc));
+    #[inline(always)]
+    pub unsafe fn entrypoint_no_alloc_impl(
+        input: *mut u8,
+        process: impl FnOnce(&Pubkey, &[AccountInfo], &[u8]) -> Result,
+    ) -> u64 {
+        let mut accounts = [const { MaybeUninit::<AccountInfo>::uninit() }; 64];
+        // SAFETY: Caller promises this is safe.
+        let parsed = unsafe { super::deserialize_into(input, &mut accounts) };
+        let (program_id, num_accounts, instruction_data) = match parsed {
+            Ok(it) => it,
+            Err(error) => return error.into(),
+        };
+        let accounts = &accounts[..num_accounts]
+            as *const [MaybeUninit<AccountInfo>]
+            as *const [AccountInfo];
+        // SAFETY: deserialize_into initialised the first num_accounts entries
+        // of the array and `MU<X>` has the same layout as `X`.
+        let accounts = unsafe { &*accounts };
+
+        // Make sure we have a new stack frame.  Solana stack frame sizes are
+        // limited so the accounts array would eat into user’s available stack
+        // space.
+        #[inline(never)]
+        fn inner(
+            program_id: &Pubkey,
+            accounts: &[AccountInfo],
+            data: &[u8],
+            process: impl FnOnce(&Pubkey, &[AccountInfo], &[u8]) -> Result,
+        ) -> Result {
+            process(program_id, accounts, data)
+        }
+
+        inner(program_id, accounts, instruction_data, process)
+            .map_or_else(|error| error.into(), |()| SUCCESS)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use solana_program::entrypoint::{
+        BPF_ALIGN_OF_U128, MAX_PERMITTED_DATA_INCREASE, NON_DUP_MARKER,
     };
 
-    check(Err(ProgramError::InvalidInstructionData), &[][..]);
-    check(Ok(&[][..]), &[0, 0, 0, 0, 1, 2, 3, 4][..]);
-    check(Ok(&[1][..]), &[1, 0, 0, 0, 1, 2, 3, 4][..]);
-    check(Err(ProgramError::InvalidInstructionData), &[1, 0, 0, 0][..]);
+    use super::*;
+
+    #[test]
+    fn test_get_ix_data() {
+        let key = Pubkey::new_unique();
+
+        fn account_info<'a>(
+            key: &'a Pubkey,
+            lamports: &'a mut u64,
+            data: &'a mut [u8],
+        ) -> AccountInfo<'a> {
+            AccountInfo::new(key, false, false, lamports, data, key, false, 0)
+        }
+
+        let check = |want, data: &[u8]| {
+            let mut lamports = 0u64;
+            let mut data = data.to_vec();
+            let acc = account_info(&key, &mut lamports, &mut data);
+            assert_eq!(want, super::get_ix_data(acc));
+        };
+
+        check(Err(ProgramError::InvalidInstructionData), &[][..]);
+        check(Ok(&[][..]), &[0, 0, 0, 0, 1, 2, 3, 4][..]);
+        check(Ok(&[1][..]), &[1, 0, 0, 0, 1, 2, 3, 4][..]);
+        check(Err(ProgramError::InvalidInstructionData), &[1, 0, 0, 0][..]);
+    }
+
+    #[derive(Debug)]
+    struct TestAccount {
+        key: Pubkey,
+        owner: Pubkey,
+        is_signer: bool,
+        is_writable: bool,
+        executable: bool,
+        rent_epoch: u64,
+        lamports: u64,
+        data: Vec<u8>,
+    }
+
+    impl TestAccount {
+        fn new(data: impl Into<Vec<u8>>) -> Self {
+            use std::sync::atomic::{AtomicU64, Ordering};
+            static CNT: AtomicU64 = AtomicU64::new(1);
+
+            let bits = CNT.fetch_add(1, Ordering::SeqCst);
+            Self {
+                key: Pubkey::new_unique(),
+                lamports: CNT.fetch_add(1, Ordering::SeqCst),
+                data: data.into(),
+                owner: Pubkey::new_unique(),
+                rent_epoch: CNT.fetch_add(1, Ordering::SeqCst),
+                is_signer: bits & 1 != 0,
+                is_writable: bits & 2 != 0,
+                executable: bits & 4 != 0,
+            }
+        }
+    }
+
+    impl PartialEq<AccountInfo<'_>> for TestAccount {
+        fn eq(&self, rhs: &AccountInfo<'_>) -> bool {
+            &self.key == rhs.key &&
+                self.lamports == rhs.lamports() &&
+                self.data.as_slice() == &rhs.try_borrow_data().unwrap()[..] &&
+                &self.owner == rhs.owner &&
+                self.rent_epoch == rhs.rent_epoch &&
+                self.is_signer == rhs.is_signer &&
+                self.is_writable == rhs.is_writable &&
+                self.executable == rhs.executable
+        }
+    }
+
+    fn serialise_input(
+        accounts: &[TestAccount],
+        instruction_data: &[u8],
+    ) -> (Pubkey, Vec<u8>, usize) {
+        let program_id = Pubkey::new_unique();
+        let mut vec = Vec::<u8>::new();
+        vec.extend_from_slice(&(accounts.len() as u64).to_le_bytes());
+
+        fn align(addr: usize) -> usize {
+            match addr % BPF_ALIGN_OF_U128 {
+                0 => 0,
+                n => BPF_ALIGN_OF_U128 - n,
+            }
+        }
+
+        for account in accounts {
+            vec.extend_from_slice(&[
+                NON_DUP_MARKER,
+                account.is_signer as u8,
+                account.is_writable as u8,
+                account.executable as u8,
+                0,
+                0,
+                0,
+                0,
+            ]);
+            vec.extend_from_slice(&account.key.as_ref());
+            vec.extend_from_slice(&account.owner.as_ref());
+            vec.extend_from_slice(&account.lamports.to_le_bytes());
+            vec.extend_from_slice(&(account.data.len() as u64).to_le_bytes());
+            vec.extend_from_slice(account.data.as_slice());
+            let align_offset = align(account.data.len());
+            let padding = MAX_PERMITTED_DATA_INCREASE + align_offset;
+            vec.resize(vec.len() + padding, 0);
+            vec.extend_from_slice(&account.rent_epoch.to_le_bytes());
+        }
+
+        vec.extend_from_slice(&(instruction_data.len() as u64).to_le_bytes());
+        vec.extend_from_slice(instruction_data);
+        vec.extend_from_slice(program_id.as_ref());
+        vec.reserve(BPF_ALIGN_OF_U128 - 1);
+
+        // Make sure the data is serialised.  We do it by inserting appropriate
+        // number of bytes at the start of the vector.
+        let pad = match vec.as_ptr().addr() % BPF_ALIGN_OF_U128 {
+            0 => 0,
+            n => {
+                vec.splice(0..0, core::iter::repeat_n(0, 8 - n));
+                8 - n
+            }
+        };
+        assert_eq!(
+            0,
+            (vec.as_ptr().wrapping_add(pad)).addr() % BPF_ALIGN_OF_U128
+        );
+
+        (program_id, vec, pad)
+    }
+
+    /// Tests whether `serialise_input` is implemented correctly.  If this test
+    /// fails, other tests are likely to fail as well.
+    #[test]
+    fn test_serialise() {
+        let accounts = [
+            TestAccount::new(b"raz"),
+            TestAccount::new(b"dwa"),
+            TestAccount::new(b"trzy"),
+            TestAccount::new(b"cztery"),
+        ];
+        let (program, mut data, offset) =
+            serialise_input(&accounts[..], b"data");
+
+        // SAFETY: Data is correctly aligned and serialised.  (We assume).
+        let (got_program, got_accounts, got_data) = unsafe {
+            solana_program::entrypoint::deserialize(
+                (&mut data[offset..]).as_mut_ptr(),
+            )
+        };
+
+        assert_eq!(&program, got_program);
+        assert_eq!(&b"data"[..], got_data);
+        assert_eq!(accounts.len(), got_accounts.len());
+        for (acc, got) in accounts.iter().zip(got_accounts.iter()) {
+            assert_eq!(acc, got);
+        }
+    }
+
+    fn do_test_entrypoint(
+        accounts: &[TestAccount],
+        instruction_data: &[u8],
+        want: Result<(usize, &[u8]), u64>,
+    ) {
+        let (program_id, mut data, offset) =
+            serialise_input(accounts, instruction_data);
+
+        let check =
+            |got_id: &Pubkey, got_accounts: &[AccountInfo], got_data: &[u8]| {
+                assert_eq!(&program_id, got_id);
+                assert_eq!(want, Ok((got_accounts.len(), got_data)));
+                for (acc, got) in accounts.iter().zip(got_accounts.iter()) {
+                    assert_eq!(acc, got);
+                }
+            };
+
+        let input = data.as_mut_ptr().wrapping_add(offset);
+        let want_result = want.clone().err().unwrap_or(0);
+        assert_eq!(want_result, unsafe {
+            __private::entrypoint_impl(input, |id, accounts, data| {
+                Ok(check(id, accounts.as_slice(), data))
+            })
+        });
+        assert_eq!(want_result, unsafe {
+            __private::entrypoint_no_alloc_impl(input, |id, accounts, data| {
+                Ok(check(id, accounts, data))
+            })
+        });
+    }
+
+    #[test]
+    fn test_entrypoint_normal() {
+        let accounts = [TestAccount::new(b"raz")];
+        do_test_entrypoint(&accounts, b"data", Ok((1, b"data")));
+    }
+
+    #[test]
+    fn test_entrypoint_staged() {
+        let data = b"\x04\x00\x00\x00data";
+        let accounts = [TestAccount::new(b"raz"), TestAccount::new(data)];
+        do_test_entrypoint(&accounts[1..], b"", Ok((0, b"data")));
+        do_test_entrypoint(&accounts, b"", Ok((1, b"data")));
+    }
+
+    #[test]
+    fn test_entrypoint_long_staged() {
+        let data = b"\x04\x00\x00\x00datagarbage";
+        let accounts = [TestAccount::new(b"raz"), TestAccount::new(data)];
+        do_test_entrypoint(&accounts[1..], b"", Ok((0, b"data")));
+        do_test_entrypoint(&accounts, b"", Ok((1, b"data")));
+    }
+
+    #[test]
+    fn test_entrypoint_short_staged() {
+        let data = b"\x04\x00\x00\x00dat";
+        do_test_entrypoint(&[TestAccount::new(data)], b"", Err(12884901888));
+    }
 }
